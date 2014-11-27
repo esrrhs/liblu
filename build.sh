ebadebf2e7dddc3f7d557d2635305440793aabe8
esrrhs <esrrhs@localhost>
2014-11-27 20:15:02 +0800

    dgdsg

---
+++ build.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+BUILD_FLAG=""
+
+if [ $# == 1 ] && [ $1 == "release" ];then
+    BUILD_FLAG=" -DREMOD=ON"
+fi
+
+#lib
+rm CMakeCache.txt -rf
+rm CMakeFiles -rf
+rm cmake_install.cmake -rf
+rm Makefile -rf
+cmake . $BUILD_FLAG
+make clean
+make -j5
+
+echo "build ok"
