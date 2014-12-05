68b824b73895ec42e88ddb67d3e45285b7341a0c
esrrhs <esrrhs@localhost>
2014-12-05 11:01:40 +0000

    aaaa

--- src/lusocket.cpp
+++ src/lusocket.cpp
@@ -821,7 +821,7 @@ int on_tcpserver_close(lutcplink * ltl)
     return 0;
 }
 
-const int encrypt_key[] = {
+const uint32_t encrypt_key[] = {
     0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x027a2a4f,
     0x706af48f, 0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4,
     0xe0d5e91e, 0x97d2d988, 0x09b64c2b, 0x7eb17cbd, 0xe7b82d07,
