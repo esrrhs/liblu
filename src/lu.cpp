6d00240c1ae67305c793742c0bdb3ac89966d656
esrrhs <esrrhs@localhost>
2014-12-05 14:51:04 +0800

    sdgasdgasdg

--- src/lu.cpp
+++ src/lu.cpp
@@ -22,15 +22,26 @@ LU_API lu * newlu(luconfig * cfg)
 	{
 		LULOG("new tcp server");
 		pl->ts = newtcpserver(pl);
+        if (!pl->ts)
+        {
+            safelufree(pl, pl);
+            return 0;
+        }
 	}
 	else if (pl->type == lut_tcpclient)
 	{
 		LULOG("new tcp client");
 		pl->tc = newtcpclient(pl);
+        if (!pl->tc)
+        {
+            safelufree(pl, pl);
+            return 0;
+        }
 	}
 	else
 	{
 		LUERR("error type %d", pl->type);
+        return 0;
 	}
 
     return pl;
@@ -88,3 +99,24 @@ LU_API void ticklu(lu * l)
 	}
 }
 
+LU_API lutype gettypelu(lu * l)
+{
+    return l->type;
+}
+
+LU_API int sendlu(lu * l, char * buffer, size_t size, int connid)
+{
+	if (l->type == lut_tcpserver)
+	{
+		return sendtcpserver(l->ts, buffer, size, connid);
+	}
+	else if (l->type == lut_tcpclient)
+	{
+		return sendtcpclient(l->tc, buffer, size, connid);
+	}
+	else
+	{
+		LUERR("error type %d", l->type);
+		return luet_typeerr;
+	}
+}
